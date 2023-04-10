37089721a0571ade94d8d9dd03e900ca8a456af7
titanium-cmd <nkansahisaac41@gmail.com>
2023-04-10 08:54:14 +0000

    login flow to homepage done

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -1,6 +1,7 @@
 
 import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 
+import { useNavigate } from 'react-router';
 import { notify } from 'reapop';
 import { useAppDispatch, useAppSelector } from 'src/hook/redux';
 import { UserCredentials } from 'src/models/user';
@@ -10,20 +11,23 @@ import LoginForm from './LoginForm';
 
 const LoginPage: React.FC = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const { status, message } = useAppSelector((state) => state.auth)
   const [credentials, setCredentials] = useState<UserCredentials>({
     email_address: '',
     password: '',
   })
 
+  console.log('status', status);
+
   useEffect(() => {
     if (status === 'rejected') {
       dispatch(notify(message, 'error'))
+      dispatch(clearAuthState())
     } else if (status === 'fulfilled') {
       dispatch(notify(message, 'success'))
-    }
-    return () => {
       dispatch(clearAuthState())
+      navigate('/')
     }
     // eslint-disable-next-line
   }, [status])
